559eecc7bfb3fce948cdf33be4ae10b1c42f39e5
Evgeny Fadeev <evgeny.fadeev@gmail.com>
2011-05-25 22:08:50 -0400

    replaced view function references with strings in the urls.py

--- categorizetags/urls.py
+++ categorizetags/urls.py
@@ -4,43 +4,42 @@ askbot askbot url configuraion file
 from django.conf.urls.defaults import url, patterns
 from django.conf.urls.defaults import handler500, handler404
 from django.contrib import admin
-from categorizetags import views
 
 admin.autodiscover()
 urlpatterns = patterns('',
     url(#ajax only
         r'^add_category/$',
-        views.add_category,
+        'categorizetags.views.add_category',
         name='add_category'
     ),
     url(#ajax only
         r'^rename_category/$',
-        views.rename_category,
+        'categorizetags.views.rename_category',
         name='rename_category'
     ),
     url(#ajax only
         r'^add_tag_to_category/$',
-        views.add_tag_to_category,
+        'categorizetags.views.add_tag_to_category',
         name='add_tag_to_category'
     ),
     url(#ajax only
         r'^get_tag_categories/$',
-        views.get_tag_categories,
+        'categorizetags.views.get_tag_categories',
         name='get_tag_categories'
     ),
     url(#ajax only
         r'^remove_tag_from_category/$',
-        views.remove_tag_from_category,
+        'categorizetags.views.remove_tag_from_category',
         name='remove_tag_from_category'
     ),
     url(#ajax only
         r'^delete_category/$',
-        views.delete_category,
+        'categorizetags.views.delete_category',
         name='delete_category'
     ),
     url(#ajax only
         r'^categories_list/$',
-        views.get_categories,
+        'categorizetags.views.get_categories',
         name='categories_list'
     ),
 )
